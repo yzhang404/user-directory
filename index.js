b890a800a2986a7ab712236171c7270b24e39446
张雨菲 <zhangyufei@zhangyufeideair.coldspring.marian.edu>
2018-05-15 16:38:08 -0400

    Append the homework 1 to user directory

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-const button = document.querySelector('button.greeting')
-function changeText(ev){
-    ev.target.textContent ="Clickty Click"
+const form = document.querySelector('#userForm')
+
+const handleSubmit = function(ev) {
+  ev.preventDefault()
+  const users = document.querySelector('#users')
+  const f = ev.target
+  const userName = f.name.value
+  const age = f.age.value
+  const favoriteColor = f.favoritecolor.value
+  //create an empty paragraph
+  const list = document.createElement('ul')
+  const nameItem = document.createElement('li')
+  nameItem.textContent = `Name: ${userName}`
+  list.appendChild(nameItem)
+
+  const ageItem = document.createElement('li')
+  ageItem.textContent = `Age: ${age}`
+  list.appendChild(ageItem)
+
+  const colorItem = document.createElement('li')
+  const colorDiv = document.createElement('div')
+  colorItem.textContent = 'Favorite Color: '
+  colorDiv.style.backgroundColor = favoriteColor
+  colorDiv.style.width = '6rem'
+  colorDiv.style.height = '3rem'
+  colorItem.appendChild(colorDiv)
+  list.appendChild(colorItem)
+
+  users.appendChild(list)
+
+  
+  f.reset()
+  f.name.focus()
 }
-button.addEventListener('click',changeText)
+
+form.addEventListener('submit', handleSubmit)
