633befc3b920e55c3c2154dbea91c7f33e363686
张雨菲 <zhangyufei@zhangyufeideair.coldspring.marian.edu>
2018-05-16 16:10:01 -0400

    Revert to original project

--- index.js
+++ index.js
@@ -7,57 +7,29 @@ const handleSubmit = function(ev) {
   const userName = f.name.value
   const age = f.age.value
   const favoriteColor = f.favoritecolor.value
-   const user={
-    userName: f.name.value,
-   age: f.age.value,
-   favoriteColor: renderColor(f.favoritecolor.value)
-  }
-
   const values = [userName,age,favoriteColor]
-  //users.appendChild(renderList(users,values))
-   users.appendChild(renderList(user))
+  users.appendChild(renderList(users,values))
   f.reset()
   f.name.focus()
 }
 
 
-//function renderList(users,values){
-  //const list = document.createElement('ul')
-  //const array = ['Name: ','Age: ','Favorite Color: ']
-  //for (i=0; i<array.length;i++){
-    //list.appendChild(renderListItem(array[i],values[i]))
-  //}
-  //return list
-//}
-
- function renderList(data){
-   const list = document.createElement('dl')
-   const labels = Object.keys(data)
-   labels.forEach( label => {
-   const item = renderListItem(label, data[label])
-   list.appendChild(item)
-  })
-   return list
-}
-function renderListItem(title,value){
-  //const item = document.createElement('li')
-  const term = document.createElement('dt')
-  term.textContent = title
-  const description = document.createElement('dd')
-
-  //if(title == 'Favorite Color: '){
-    //  return renderColor(item,value)
-  //} else{
-    //item.textContent = `${title} ${value}`
-  //}
-
-  try{
-    description.appendChild(value)
-  } catch(e){
-    description.textContent += value
+function renderList(users,values){
+  const list = document.createElement('ul')
+  const array = ['Name: ','Age: ','Favorite Color: ']
+  for (i=0; i<array.length;i++){
+    list.appendChild(renderListItem(array[i],values[i]))
   }
-  item.appendChild(term)
-  item.appendChild(description)
+  return list
+}
+
+function renderListItem(title,value){
+  const item = document.createElement('li')
+  if(title == 'Favorite Color: '){
+      return renderColor(item,value)
+  } else{
+    item.textContent = `${title} ${value}`
+  }
   return item
 }
 function renderColor(colorItem,favoriteColor){
@@ -70,7 +42,3 @@ function renderColor(colorItem,favoriteColor){
   return colorItem
 }
 form.addEventListener('submit', handleSubmit)
-
-// const doTheThing = () =>{
-// another way of writing functions
-//}
